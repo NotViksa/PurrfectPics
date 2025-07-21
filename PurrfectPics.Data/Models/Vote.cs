4c768ceea5269d4c022ad4a56669f0770087cd27
NotViksa <viki.10019@gmail.com>
2025-07-21 20:55:13 +0300

    updated Vote.cs

--- PurrfectPics.Data/Models/Vote.cs
+++ PurrfectPics.Data/Models/Vote.cs
@@ -1,17 +1,21 @@
 ﻿using PurrfectPics.Data.Models.Identity;
+using System;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace PurrfectPics.Data.Models
 {
     public class Vote
     {
         public int Id { get; set; }
-        public bool IsUpvote { get; set; } // true = upvote, false = downvote
+        public bool IsUpvote { get; set; }
         public DateTime VotedDate { get; set; } = DateTime.UtcNow;
 
+        // Foreign Keys
         public int CatImageId { get; set; }
-        public CatImage CatImage { get; set; }
-
         public string UserId { get; set; }
+
+        // Navigation Properties
+        public CatImage CatImage { get; set; }
         public ApplicationUser User { get; set; }
     }
 }
